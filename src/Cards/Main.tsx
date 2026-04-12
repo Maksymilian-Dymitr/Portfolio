16169992d5125ef50dfb92d8e6d2048709862389
IIMaksymilianDymitrII <maksymilian.dymitr@chasacademy.se>
2026-04-12 22:56:19 +0200

    Made the home section responsive

--- src/Cards/Main.tsx
+++ src/Cards/Main.tsx
@@ -5,34 +5,32 @@ import pic from "../pic.jpg"
 
 export const Main = forwardRef<HTMLDivElement, Props>(
   ({ isVisible, id }, mainRef) => {
-    //[#7c3aed]
     return (
       <Card>
         <div
           id={id}
           ref={mainRef}
           className={`w-full
-            h-full flex justify-between items-center transition-all duration-700
+            h-full flex md:flex-row flex-col-reverse 
+            justify-center items-center transition-all duration-700
           ${isVisible ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
         >
-          <div className="p-12 w-[40%] flex flex-col items-center ">
-            <h1 className="p-7 text-6xl font-bold">
-              Maksymilan Dymitr
+          <div className=" md:w-[40%] flex flex-col items-center ">
+            <h1 className="p-7 md:text-6xl text-4xl font-bold text-center">
+              Maksymilian Dymitr
             </h1>
-            <div className="bg-[#292929] rounded-2xl px-4 py-2 font-medium">
-            <p
-              className="text-3xl w-fit 
-              rounded-2xl "
+            <div className="bg-[#292929] rounded-xl px-4 py-2 font-medium">
+            <p className="md:text-3xl rounded-2xl text-2xl"
             >
               Full-Stack Developer
             </p>
             </div>
             
           </div>
-          <div className="w-[60%] flex justify-center">
-            <div className="w-fit bg-zinc-800 rounded-full pt-2 overflow-hidden
+          <div className="w-[80vw] flex justify-center items-center">
+            <div className="bg-zinc-800 rounded-full overflow-hidden
             ring-white/10 ring-2 "> 
-              <img src={pic} className="w-[30vw] h-[38vw] 
+              <img src={pic} className="md:w-[30vw] md:h-[38vw] w-dvw
               object-cover "></img>
           </div>
           </div>
