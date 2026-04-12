9cbda4622c36f22a17a5eb99d14c5f6ef413a46a
IIMaksymilianDymitrII <maksymilian.dymitr@chasacademy.se>
2026-04-12 22:38:38 +0200

    added grids in projects

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 - [x] Create a Logo Loop for technologies in about 
 - [x] Make animation when hover on buttons 
-- [ ] Write More in about
+- [x] Write More in about
 - [ ] Create grid of projects for projects with a status dot
-- [ ] Simplify the cards by seperating them as components
 - [ ] Make site responsive
+- [ ] Simplify the cards by seperating them as components
--- src/Cards/Projects.tsx
+++ src/Cards/Projects.tsx
@@ -10,15 +10,32 @@ export const Projects = forwardRef<HTMLDivElement, Props>(
           ref={projectRef}
           id={id}
           className={`w-full h-full flex snap-center
-            transition-all duration-700 flex-col 
+            transition-all duration-700 flex-col
           ${isVisible ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
         >
-          <h1 className="pt-10 pl-10 text-6xl font-bold " >
-            Projects
-          </h1>
-          <div className="font-bold text-9xl flex justify-center items-center w-full h-full"> 
-            <h1>
-              Coming Soon 😪
+          <h1 className="pt-10 pl-10 text-6xl font-bold ">Projects</h1>
+          <div className="flex justify-center items-center">
+            <div className="grid p-10 grid-cols-2 gap-10 ">
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+              <div className="w-[43vw] bg-[#1e1e1f] border border-white/5
+             h-[15vh] rounded-xl hover:border-white/15 duration-500" ></div>
+            </div>
+            <h1 className="fixed text-9xl font-bold bg-[#1e1e1f] border 
+              rounded-xl p-10 border-whtie/5">
+              Coming Soon 😴
             </h1>
           </div>
         </div>
