f4824d2b005ecbac12eee9364d33bc030c1ce48b
IIMaksymilianDymitrII <maksymilian.dymitr@chasacademy.se>
2026-04-12 23:08:16 +0200

    Corrected responsive design in home section

--- src/Cards/Main.tsx
+++ src/Cards/Main.tsx
@@ -11,12 +11,13 @@ export const Main = forwardRef<HTMLDivElement, Props>(
           id={id}
           ref={mainRef}
           className={`w-full
-            h-full flex md:flex-row flex-col-reverse 
+            h-full flex lg:flex-row flex-col-reverse 
             justify-center items-center transition-all duration-700
           ${isVisible ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10"}`}
         >
-          <div className=" md:w-[40%] flex flex-col items-center ">
-            <h1 className="p-7 md:text-6xl text-4xl font-bold text-center">
+          <div className=" lg:w-[50%] flex flex-col items-center ">
+            <h1 className="p-6 mb-0 sm:mb-3 sm:text-6xl text-4xl font-bold text-center
+             leading-11 lg:leading-17">
               Maksymilian Dymitr
             </h1>
             <div className="bg-[#292929] rounded-xl px-4 py-2 font-medium">
@@ -30,7 +31,7 @@ export const Main = forwardRef<HTMLDivElement, Props>(
           <div className="w-[80vw] flex justify-center items-center">
             <div className="bg-zinc-800 rounded-full overflow-hidden
             ring-white/10 ring-2 "> 
-              <img src={pic} className="md:w-[30vw] md:h-[38vw] w-dvw
+              <img src={pic} className="lg:w-[30vw] lg:h-[38vw] w-dvw
               object-cover "></img>
           </div>
           </div>
